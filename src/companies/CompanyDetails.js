59201ab50bfe26a3336bb801743fd594f8a7b898
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-06-04 22:17:13 -0600

    beginning work on company details route

--- src/companies/CompanyDetails.js
+++ src/companies/CompanyDetails.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import JoblyApi from "../api/api";
+import LoadingSpinner from "../common/LoadingSpinner";
 
 function CompanyDetails() {
   const { handle } = useParams();
+  const [company, setCompany] = useState(null);
+
+  useEffect(() => {
+    async function getCompanyDetails() {
+      setCompany(await JoblyApi.getCompany(handle));
+    }
+    getCompanyDetails();
+  }, [handle]);
+
+  if (!company) return <LoadingSpinner />;
 
   return (
     <div>
-      <h1>These are the company details for {handle}</h1>
+      <h1>These are the company details for {company.name}</h1>
     </div>
   );
 }
